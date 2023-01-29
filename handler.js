e16fd0ea2670fbcc954f7fcd3c5f342fff1dc503
Camilo Cunha de Azevedo <camilotk@gmail.com>
2023-01-29 19:06:51 -0300

    Refactor with Ramda

--- handler.js
+++ handler.js
@@ -3,9 +3,9 @@
 const { GoogleSpreadsheet } = require('google-spreadsheet')
 const clientSecret = require('./client_secret.json')
 const yup = require('yup')
+const R = require('ramda')
 
-const validate = async (body) => {
-  const schema = yup.object().shape({
+const schema = yup.object().shape({
     name: yup.string().required(),
     title: yup.string().required(),
     description: yup.string().required(),
@@ -14,85 +14,62 @@ const validate = async (body) => {
     bio: yup.string().required(),
     social: yup.string().required(),
     email: yup.string().email().required(),
-  })
+})
 
-  return await schema.isValid(body)
-}
+const validate = body => schema.isValid(body)
+
+const handleError = R.curry((event, e) => {
+  console.error({
+    tag: '[FATAL ERROR]',
+    metadata: {
+      event: JSON.stringify(event),
+      message: e?.message,
+      stack: e?.stack,
+    },
+  })
+  return {
+    statusCode: 500,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true,
+    },
+  }
+})
 
 const cfp = async (event) => {
-  try {
-    const googleSheetID = '1u6BXqKLXojnyS8wHESGhYM3YkddBN44hmv166ZzKU44'
-    const sheet = new GoogleSpreadsheet(googleSheetID)
-    await sheet.useServiceAccountAuth(clientSecret)
-    await sheet.loadInfo()
-
-    const tab = sheet.sheetsByTitle['CFP']
-
-    await tab.loadHeaderRow()
-
-    const body = JSON.parse(event.body)
-
-    const isValid = await validate(body)
-
-    if (!isValid) {
-      console.error({
-        tag: '[INVALID BODY]',
-        metadata: { body },
-      })
-  
-      return {
-        statusCode: 422,
-        headers: {
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Credentials': true,
-        },  
-        body: JSON.stringify({
-          message: 'Invalid request',
-        }),
-      }
+    try {
+        const body = JSON.parse(event.body)
+        const validation = await validate(body)
+        if(validation){
+            const googleSheetID = '1u6BXqKLXojnyS8wHESGhYM3YkddBN44hmv166ZzKU44'
+            const sheet = new GoogleSpreadsheet(googleSheetID)
+            await sheet.useServiceAccountAuth(clientSecret)
+            await sheet.loadInfo()
+            const tab = sheet.sheetsByTitle['CFP']
+            await tab.loadHeaderRow()
+            await tab.addRow(body)
+            return {
+              headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Credentials': true,
+              },
+              statusCode: 200,
+            }
+        }else {
+            return {
+                statusCode: 422,
+                headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Credentials': true,
+                },
+                body: JSON.stringify({
+                message: 'Invalid request',
+                }),
+            }
+        }
+    } catch (error) {
+        return handleError(event)(error)
     }
-
-    await tab.addRow(body)
-
-    return {
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Credentials': true,
-      },
-      statusCode: 200,
-    }
-  } catch (e) {
-    console.error({
-      tag: '[FATAL ERROR]',
-      metadata: {
-        event: JSON.stringify(event),
-        message: e?.message,
-        stack: e?.stack,
-      },
-    })
-
-    return {
-      statusCode: 500,
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Credentials': true,
-      },
-    }
-  }
 }
 
-// cfp({ input: { body: `{
-//   "name": "@bmacabeus",
-//   "talk title": "gameboy advance",
-//   "description": "kekekekek kekek sdf sdf sjfshjk shfjk fdsafsfsf fsafsdfs",
-//   "talk duration": 30,
-//   "format": "in-person",
-//   "bio": "fdfsd fsfsd fsf sfda fsafdfs",
-//   "social medias": "twitter\\nfacebook\\ninstagram",
-//   "email": "bruno.macabeus@gmail.com"
-// }
-// ` } }).catch((e) => {
-//   debugger
-// })
-
-module.exports.cfp = cfp
+module.exports = {cfp}
