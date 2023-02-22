30253dce736c5a0c954f205cc5209a1bb4719d59
Camilo Cunha de Azevedo <camilotk@gmail.com>
2023-02-22 03:03:29 -0300

    Unfinished work

--- handler.js
+++ handler.js
@@ -3,6 +3,10 @@
 const { GoogleSpreadsheet } = require('google-spreadsheet')
 const clientSecret = require('./client_secret.json')
 const yup = require('yup')
+const AWS = require('aws-sdk')
+
+AWS.config.update({region: clientSecret.region})
+const ses = new AWS.SES({apiVersion: '2010-12-01'})
 
 const validate = async (body) => {
   const schema = yup.object().shape({
@@ -17,6 +21,41 @@ const validate = async (body) => {
   })
 
   return await schema.isValid(body)
+}
+
+const sendMail = async (body) => {
+    const params = {
+      Source: 'bruno.macabeus@gmail.com',
+      Destination: { 
+        ToAddresses: [
+          body.email
+        ]
+      },
+      Message: {
+        Body: {
+          Text: {
+            Data: 'kekekekek kekek sdf sdf sjfshjk shfjk fdsafsfsf fsafsdfs'
+          }
+        },
+        Subject: {
+          Data: 'CFP Gambiconf'
+        }
+      }
+    }
+    
+    try {
+      const sendPromise = await ses.sendEmail(params).promise()
+      return {
+        statusCode: 200,
+        body: "Email sent"
+      }
+    } catch (error) {
+      console.log(error)
+      return {
+        statusCode: 500,
+        body: "Error " + error
+      }
+    }
 }
 
 const cfp = async (event) => {
@@ -53,7 +92,9 @@ const cfp = async (event) => {
     }
 
     await tab.addRow(body)
-
+    const result = await sendMail(body)
+    console.log(result)
+    
     return {
       headers: {
         'Access-Control-Allow-Origin': '*',
